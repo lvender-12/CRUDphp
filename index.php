30484484d054ca9f2899aa74b2d5a7d2c83e5e25
Lvender <muhamaddamar1812@gmail.com>
2025-06-16 18:59:44 +0700

    navbar

--- index.php
+++ index.php
@@ -3,11 +3,30 @@
 <head>
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css" integrity="sha384-zCbKRCUGaJDkqS1kPbPd7TveP5iyJE0EjAuZQTgFLD2ylzuqKfdKlfG/eSrtxUkn" crossorigin="anonymous">
 </head>
-<title>
-    CRUD</title>
+<title>kayaknya</title>
 <body>
-    <nav class="navbar navbar-dark bg-dark">
-            <span class="navbar-brand mb-0 h1">CRUD</span>
+    <nav class="navbar navbar-expand-lg bg-body-tertiary bg-dark border-body">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#">Project apa aja</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item">
+                <a class="nav-link disabled" aria-disabled="true">Disabled</a>
+                </li>
+            </ul>
+            </div>
         </div>
     </nav>
 <div class="container">
